99ea9ba2e8e13840afeb792622cd86cc041ddb2c
nilesh94 <nilesh.sandil@live.in>
2026-01-07 14:01:02 +0530

    Update utils.py

--- app/users/utils.py
+++ app/users/utils.py
@@ -1,14 +1,23 @@
-# File: app/users/utils.py
-
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
 
-# Setup bcrypt password hashing
+# CONFIG (Move to .env in production)
+SECRET_KEY = "CHANGE_THIS_TO_A_SUPER_SECRET_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str) -> str:
-    """Takes a plain password and returns a hashed string."""
+def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Checks if a plain password matches the hash (For Login)."""
+def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
