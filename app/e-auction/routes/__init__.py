c4fc019dc82ace92948a88f7156d845295dbb487
nilesh94 <nilesh.sandil@live.in>
2026-02-01 17:59:27 +0530

    Update __init__.py

--- app/e-auction/routes/__init__.py
+++ app/e-auction/routes/__init__.py
@@ -1,1 +1,24 @@
+"""
+E-Auction Routes Package
+Export all API routers
+"""
 
+from .auctions import router as auctions_router
+from .bidding import router as bidding_router
+from .payments_participants import payment_router, participant_router
+
+# Collect all routers
+all_routers = [
+    auctions_router,
+    bidding_router,
+    payment_router,
+    participant_router,
+]
+
+__all__ = [
+    "auctions_router",
+    "bidding_router",
+    "payment_router",
+    "participant_router",
+    "all_routers",
+]
